bdcc1369c7b6887e5542e434d412b4e8b5f57496
benoit74 <benoit74@users.noreply.github.com>
2023-06-29 14:12:49 +0200

    Add axios with dummy request

--- frontend/src/stores/main.js
+++ frontend/src/stores/main.js
@@ -1,13 +1,27 @@
 import { defineStore } from 'pinia'
-
+import axios from "axios"
 export const useMainStore = defineStore('main', {
-  state: () => ({ count: 0 }),
+  state: () => ({
+    count: 0,
+    users: [],
+  }),
   getters: {
     doubleCount: (state) => state.count * 2,
+    getUsers: (state) => state.users,
   },
   actions: {
     increment() {
       this.count++
     },
+    async fetchUsers() {
+      try {
+        const data = await axios.get('https://jsonplaceholder.typicode.com/users')
+        this.users = data.data
+      }
+      catch (error) {
+        alert(error)
+        console.log(error)
+      }
+    }
   },
 })
--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,7 +1,11 @@
 <script setup>
 import { useMainStore } from '../stores/main'
-
+import { onMounted } from "vue";
 const main = useMainStore()
+
+onMounted(() => {
+  main.fetchUsers();
+});
 
 </script>
 
@@ -9,6 +13,10 @@ const main = useMainStore()
   An empty about page (count is {{ main.count }}, double is {{ main.doubleCount }})
   <br><router-link to="/">Go to home</router-link>
   <br><router-link to="/about2">Go to About 2</router-link>
+
+  <div v-for="user in main.users" :key="user.id">
+    {{ user.id }}: {{ user.name }} @ {{ user.address.city }}
+  </div>
 </template>
 
 <style scoped></style>
