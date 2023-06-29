8b40e7eba9b13fa26f3efe082b8ee66b99a8594d
benoit74 <benoit74@users.noreply.github.com>
2023-06-29 13:51:24 +0200

    Add bootstrap with dummy content

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -7,9 +7,8 @@ const main = useMainStore()
 
 <template>
   An empty about page (count is {{ main.count }}, double is {{ main.doubleCount }})
-  <router-link to="/">Go to home</router-link>
-  <router-link to="/about2">Go to About 2</router-link>
+  <br><router-link to="/">Go to home</router-link>
+  <br><router-link to="/about2">Go to About 2</router-link>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
