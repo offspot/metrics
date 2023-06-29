dbd5ea8bb7c75e023c5d4eedb54e3627236731d6
benoit74 <benoit74@users.noreply.github.com>
2023-06-29 16:50:20 +0200

    Minimalistic dashboard diplaying KPIs

--- backend/src/backend/routes/kpis.py
+++ backend/src/backend/routes/kpis.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
@@ -47,3 +47,36 @@ def kpis_inner(agg_kind: str, agg_value: str, session: Session) -> List[Kpi]:
             session.execute(query),
         )
     )
+
+
+# TODO: specify the 404 response model
+@router.get(
+    "/{kpi_id}",
+    responses={
+        200: {
+            "description": "Returns the kpi value for a given aggregation",
+        },
+        404: {"description": "KPI not found"},
+    },
+)
+async def kpi(kpi_id: str, agg_kind: str, agg_value: str) -> Kpi | None:
+    return kpi_inner(kpi_id=kpi_id, agg_kind=agg_kind, agg_value=agg_value)
+
+
+@dbsession
+def kpi_inner(kpi_id: str, agg_kind: str, agg_value: str, session: Session) -> Kpi:
+    query = (
+        select(KpiValue.kpi_id, KpiValue.kpi_value)
+        .where(KpiValue.kpi_id == kpi_id)
+        .where(KpiValue.agg_value == agg_value)
+        .where(KpiValue.agg_kind == agg_kind)
+    )
+
+    item = session.execute(query).first()
+    if not item:
+        raise HTTPException(
+            status_code=404,
+            detail="KPI not found",
+        )
+
+    return Kpi(id=item.kpi_id, value=item.kpi_value)
--- frontend/src/stores/main.js
+++ frontend/src/stores/main.js
@@ -2,26 +2,90 @@ import { defineStore } from 'pinia'
 import axios from "axios"
 export const useMainStore = defineStore('main', {
   state: () => ({
-    count: 0,
-    users: [],
+    aggregations: [],
+    selected_agg_kind: null,
+    agg_values_for_selected_agg_kind: [],
+    selected_agg_value: null,
+    selected_kpi_id: null,
+    value_for_selected_kpi_id: null,
+    is_loading: false,
   }),
   getters: {
-    doubleCount: (state) => state.count * 2,
-    getUsers: (state) => state.users,
+    getAggregations: (state) => {
+      return (kind) => state.aggregations.filter(aggregation => aggregation.kind === kind)
+    },
   },
   actions: {
-    increment() {
-      this.count++
+    handle_agg_kind_updated() {
+      this.agg_values_for_selected_agg_kind = this.getAggregations(this.selected_agg_kind).map(agg => agg.value)  
+      this.selected_agg_value = this.agg_values_for_selected_agg_kind[this.agg_values_for_selected_agg_kind.length - 1]
+      this.fetchKpi()
     },
-    async fetchUsers() {
+    update_kpi(value) {
+      switch (value) {
+        case 'Top contents':
+          this.selected_kpi_id = 1
+          break;
+        case 'Top 50 resources':
+          this.selected_kpi_id = 2
+          break;
+        default:
+          console.log(`Unsupported KPI: '${value}'.`);
+          return
+      }
+      this.fetchKpi()
+    },
+    update_agg_kind(kind) {
+      switch (kind) {
+        case 'Daily':
+          this.selected_agg_kind = 'D'
+          break;
+        case 'Weekly':
+          this.selected_agg_kind = 'W'
+          break;
+        case 'Monthly':
+          this.selected_agg_kind = 'M'
+          break;
+        case 'Yearly':
+          this.selected_agg_kind = 'Y'
+          break;
+        default:
+          console.log(`Unsupported aggregation kind: '${kind}'.`);
+          return
+      }
+      this.handle_agg_kind_updated()
+    },
+    update_agg_value(value) {
+      this.selected_agg_value = value
+      this.fetchKpi()
+    },
+    async fetchAggregations() {
+      this.is_loading = true
       try {
-        const data = await axios.get('https://jsonplaceholder.typicode.com/users')
-        this.users = data.data
+        const data = await axios.get('http://localhost:8002/v1/aggregations')
+        this.aggregations = data.data
+        this.handle_agg_kind_updated()
       }
       catch (error) {
         alert(error)
         console.log(error)
       }
-    }
+      this.is_loading = false
+    },
+    async fetchKpi() {
+      this.is_loading = true
+      if (!this.selected_kpi_id || !this.selected_agg_kind || !this.selected_agg_value) {
+        return
+      }
+      try {
+        const data = await axios.get('http://localhost:8002/v1/kpis/' + this.selected_kpi_id + '?agg_kind=' + this.selected_agg_kind + '&agg_value=' + this.selected_agg_value )
+        this.value_for_selected_kpi_id = data.data.value
+      }
+      catch (error) {
+        alert(error)
+        console.log(error)
+      }
+      this.is_loading = false
+    },
   },
 })
