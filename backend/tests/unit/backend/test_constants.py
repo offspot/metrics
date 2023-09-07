719d369382eb15101921f2d9e478b826442d99da
benoit74 <benoit74@users.noreply.github.com>
2023-09-07 16:23:06 +0200

    Fix number of / in DB URLs

--- backend/tests/unit/backend/test_constants.py
+++ backend/tests/unit/backend/test_constants.py
@@ -2,4 +2,4 @@ from offspot_metrics_backend.constants import BackendConf
 
 
 def test_db_api() -> None:
-    assert BackendConf.database_url.startswith("sqlite+pysqlite:///")
+    assert BackendConf.database_url.startswith("sqlite+pysqlite:////")
