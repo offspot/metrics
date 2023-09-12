5aa984a6420a79697dab49680fa447c066b5857b
benoit74 <benoit74@users.noreply.github.com>
2023-09-12 17:03:30 +0200

    Enhance AggKind enum typing + documentation

--- backend/src/offspot_metrics_backend/business/agg_kind.py
+++ backend/src/offspot_metrics_backend/business/agg_kind.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 
-class AggKind(Enum):
+class AggKind(str, Enum):
     """The various kind of supported aggregations"""
 
     DAY = "D"
@@ -11,8 +11,10 @@ class AggKind(Enum):
 
     @classmethod
     def values(cls) -> list[str]:
-        return [e.value for e in AggKind]
+        """Returns all enum allowed values"""
+        return [item.value for item in AggKind]
 
     @classmethod
     def pattern(cls) -> str:
+        """Returns a regexp matching enum allowed values"""
         return f'^{"|".join(cls.values())}$'
