0ad57d23ba1a3e8cbaf90e041d05fbfc3b074f54
benoit74 <benoit74@users.noreply.github.com>
2023-11-24 15:03:59 +0100

    Make a distinction between clock input tick and background processing tick

--- backend/src/offspot_metrics_backend/main.py
+++ backend/src/offspot_metrics_backend/main.py
@@ -58,16 +58,23 @@ class Main:
             self.config.parse_configuration()
             self.converter = CaddyLogConverter(self.config)
             self.processor.startup(current_period=Period.now().period)
+
             log_watcher_task = create_task(self.start_watcher())
             self.background_tasks.add(log_watcher_task)
             log_watcher_task.add_done_callback(
                 functools.partial(self.task_stopped, "Log Watcher")
             )
 
-            ticker_task = create_task(self.ticker())
-            self.background_tasks.add(ticker_task)
-            ticker_task.add_done_callback(
-                functools.partial(self.task_stopped, "Ticker")
+            input_ticker_task = create_task(self.input_ticker())
+            self.background_tasks.add(input_ticker_task)
+            input_ticker_task.add_done_callback(
+                functools.partial(self.task_stopped, "Input ticker")
+            )
+
+            processing_ticker_task = create_task(self.processing_ticker())
+            self.background_tasks.add(processing_ticker_task)
+            processing_ticker_task.add_done_callback(
+                functools.partial(self.task_stopped, "Processing ticker")
             )
         else:
             logger.warning("Processing is disabled")
@@ -77,30 +84,57 @@ class Main:
         if self.log_watcher:
             self.log_watcher.stop()
 
-    async def start_watcher(self):
-        """Start the log watcher as a coroutine"""
-        if not self.log_watcher:
-            raise ValueError("Log watcher has not been initialized")
-        await self.log_watcher.run_async()
-
     def task_stopped(self, task_name: str, task: Task[Any]) -> None:
+        if task.cancelled():
+            return
         exc = task.exception()
         self.background_tasks.discard(task)
         if exc:
             logger.error(f"{task_name} has stopped anormally", exc_info=exc)
             sys.exit(1)
 
-    async def ticker(self):
-        """Start a processor tick every minute"""
+    async def start_watcher(self):
+        """Start the log watcher as a coroutine"""
+        if not self.log_watcher:
+            raise ValueError("Log watcher has not been initialized")
+        await self.log_watcher.run_async()
+
+    async def input_ticker(self):
+        """Generate a ClockTick input every minute
+
+        We need this input to be as precisely as possible generated every
+        minute, i.e. 60 times per hour. Currently we assume that the logic
+        processing input is fast enough to take less than one minute (if it
+        does take longer, we have many other issues anyway so no need to
+        overwhelm the system with 60 inputs per hour).
+        """
+        while True:
+            _, now_datetime = Period.now()
+            await sleep(TICK_PERIOD - now_datetime.second)
+            logger.debug("Generating a ClockTick input")
+            try:
+                _, now_datetime = Period.now()
+                self.processor.process_input(ClockTick(ts=now_datetime))
+            except Exception as ex:
+                logger.debug("Exception occured in clock tick", exc_info=ex)
+
+    async def processing_ticker(self):
+        """Start processing cycles with one minute pauses between them
+
+        We do not what / need something precise here because we want to let
+        the system "breath" between processing cycle and the processing logic
+        does not mind if there is not 60 cycles per hour, it just need to
+        regularly persist data in DB + perform needed computation every hour.
+        """
         while True:
             await sleep(TICK_PERIOD)
-            logger.debug("Processing a clock tick")
+            logger.debug("Background processing started")
             try:
-                now_period, now_datetime = Period.now()
-                self.processor.process_input(ClockTick(ts=now_datetime))
+                now_period, _ = Period.now()
                 self.processor.process_tick(tick_period=now_period)
             except Exception as ex:
                 logger.debug("Exception occured in clock tick", exc_info=ex)
+            logger.debug("Background processing completed")
 
     def handle_log_event(self, event: NewLineEvent):
         logger.debug(f"Log watcher sent: {event.line_content}")
