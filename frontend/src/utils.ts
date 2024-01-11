be3e48d86331ee9b26bb1f2dff767bd72b8b9469
benoit74 <benoit74@users.noreply.github.com>
2024-01-11 10:42:18 +0100

    Choose colors in a nice looking color scheme

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,11 +1,30 @@
 /*
- * Get a random Hex color like "#32FF4D", to be used in CSS
+ * Get a random Hex color in our color scheme, to be used in CSS
  */
 export const getRandomColor = (): string => {
-  const letters = '0123456789ABCDEF'
-  let color = '#'
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)]
-  }
-  return color
+  /*
+    Color scheme is from https://sashamaps.net/docs/resources/20-colors/
+    We selected colors which are differentiable for 99% of the population and just didn't used
+    the white which was hard to see in our design.
+  */
+  const colorScheme = [
+    '#e6194B',
+    '#3cb44b',
+    '#ffe119',
+    '#4363d8',
+    '#f58231',
+    '#42d4f4',
+    '#f032e6',
+    '#fabed4',
+    '#469990',
+    '#dcbeff',
+    '#9A6324',
+    '#fffac8',
+    '#800000',
+    '#aaffc3',
+    '#000075',
+    '#a9a9a9',
+    '#000000',
+  ]
+  return colorScheme[Math.floor(Math.random() * colorScheme.length)]
 }
