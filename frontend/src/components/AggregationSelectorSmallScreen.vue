4afc54266625a7bacada5aab2fe803a702ee2133
benoit74 <benoit74@users.noreply.github.com>
2024-02-01 14:47:13 +0100

    Revisit mobile aggregation selector for bigger layout

--- frontend/src/components/AggregationSelectorSmallScreen.vue
+++ frontend/src/components/AggregationSelectorSmallScreen.vue
@@ -9,34 +9,33 @@ const toggleOpen = () => {
   isOpen.value = !isOpen.value
 }
 
-const height = computed(() => (isOpen.value ? '200px' : '15px'))
+const height = computed(() => (isOpen.value ? '260px' : '15px'))
 </script>
 
 <template>
   <div id="selector1" :style="{ height: height }">
     <div v-if="isOpen">
-      <div id="select-timeline" class="pt-2 pb-2">Select timeline</div>
+      <div id="select-timeline" class="pt-4 pb-4">Select timeline</div>
       <div id="agg-selector">
         <v-btn-toggle
           divided
           mandatory
-          density="compact"
           rounded
           :model-value="mainStore.aggregationKind"
           @update:model-value="
             (newValue) => mainStore.setAggregationKind(newValue)
           "
         >
-          <v-btn value="D"> Day </v-btn>
+          <v-btn size="large" value="D"> Day </v-btn>
 
-          <v-btn value="W"> Week </v-btn>
+          <v-btn size="large" value="W"> Week </v-btn>
 
-          <v-btn value="M"> Month </v-btn>
+          <v-btn size="large" value="M"> Month </v-btn>
 
-          <v-btn value="Y"> Year </v-btn>
+          <v-btn size="large" value="Y"> Year </v-btn>
         </v-btn-toggle>
       </div>
-      <div class="pt-4 pb-4 font-weight-bold">
+      <div class="pt-6 pb-6 font-weight-bold">
         <div v-if="mainStore.hasAggregationValues">
           <span class="text-subtitle-1 font-weight-medium">{{
             mainStore.date_part_1
@@ -55,6 +54,7 @@ const height = computed(() => (isOpen.value ? '200px' : '15px'))
           min-width="10em"
           prepend-icon="fas fa-arrow-left"
           variant="flat"
+          size="large"
           :disabled="!mainStore.hasPrevAggregationValue"
           @click="mainStore.toPreviousAggregrationValue"
         >
@@ -65,6 +65,7 @@ const height = computed(() => (isOpen.value ? '200px' : '15px'))
           min-width="10em"
           append-icon="fas fa-arrow-right"
           variant="flat"
+          size="large"
           :disabled="!mainStore.hasNextAggregationValue"
           @click="mainStore.toNextAggregationValue"
         >
@@ -107,13 +108,6 @@ const height = computed(() => (isOpen.value ? '200px' : '15px'))
   padding-top: 0.5em;
   padding-bottom: 0.5em;
 }
-#select-timeline {
-  width: 300px;
-  text-align: left;
-  margin-left: auto;
-  margin-right: auto;
-  font-size: 0.9rem !important;
-}
 
 #apply-btn {
   letter-spacing: 0.1em;
@@ -143,5 +137,6 @@ const height = computed(() => (isOpen.value ? '200px' : '15px'))
 
 #prev-next .v-btn--disabled {
   background-color: #e2e1e4;
+  color: #7266ed;
 }
 </style>
